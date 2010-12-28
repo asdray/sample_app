c97fba1b8eb390d8d23055c0cc96efc762277848
Alan S. Dray <adray@rochester.rr.com>
2010-12-28 15:00:50 -0500

    Finished first cut of the User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SampleApp::Application.routes.draw do
+  resources :users
 
-  get "users/new"
+  #get "users/new"
 
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
@@ -8,7 +9,7 @@ SampleApp::Application.routes.draw do
   match '/signup',  :to => 'users#new'
 
   root :to => "pages#home"
-
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
